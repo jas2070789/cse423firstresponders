cc58ac859956ccd8e6c57f37070c87ad901bbe2c
Patrick Archer <37312105+pjarcher913@users.noreply.github.com>
2019-03-28 13:06:42 -0700

    Update server.py

--- BACKEND/configure_adhoc/server.py
+++ BACKEND/configure_adhoc/server.py
@@ -18,20 +18,20 @@ sock.bind((ADHOC_IP, UDP_PORT)) # this only binds 10.0.0.1 to port 5005
 
 # set server to constantly be listening for data transmitted to the configured socket
 while True:
-	# receive data and IP address from sender
+    # receive data and IP address from sender
     data, addr = sock.recvfrom(1024)
 	
-	# for debug
+    # for debug
     print "received message:", data
     
-	# save corresponding transmitted data (ie, the  IP address and port of client)
+    # save corresponding transmitted data (ie, the  IP address and port of client)
     return_addr = addr[0]
     return_port = addr[1]
     
-	# for debug
+    # for debug
     print "Return address: ", str(return_addr)
     print "Return port: ", str(return_port)
     
-	# return a test message to sender/client
+    # return a test message to sender/client
     return_message = "I got your message."
     sock.sendto(return_message, (return_addr, return_port))
