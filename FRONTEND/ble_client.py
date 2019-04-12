dc0833a8d41f5c9f9131c016b96aa1f8ff6a83fc
HaoyuFoogueir <38366418+HaoyuFoogueir@users.noreply.github.com>
2019-04-12 11:48:34 -0700

    Update ble_client.py

--- FRONTEND/ble_client.py
+++ FRONTEND/ble_client.py
@@ -3,11 +3,14 @@ from bluepy.btle import Scanner, DefaultDelegate
 from bluepy.btle import UUID, Peripheral
 import bluepy.btle
 
-device_name = "Fooguier X4"
+#modify the following constants according to the config of the wearable
+device_name = "Fooguier X4"	
 device_addr = "none"
+device_addr_type = "random"
 device_char_handle = "First Name"
 device_char_UUID = 0x2A8A
-char_read_interval = 1	#second
+char_read_interval = 1	#time inverval of reading data from the wearable, in [second]
+
 print("Program running")
 
 #scan target device by name
@@ -23,11 +26,11 @@ class ScanDelegate(DefaultDelegate):
 			
 scanner = Scanner().withDelegate(ScanDelegate())
 
+#looking for device using device name
 device_found = 0
-
 while device_found == 0:
-	devices = scanner.scan(0.35)
 	
+	devices = scanner.scan(0.35)	
 	for ii in devices:
 		print(ii.addr, ii.getValueText(9))
 		if ii.getValueText(9) == device_name:
@@ -38,18 +41,20 @@ print("device found")
 print("device address: ", device_addr)
 
 #connect to device
-p = Peripheral(device_addr, "random", 0)
+p = Peripheral(device_addr, device_addr_type, 0)
+
+#modify if multiple characteristics are available
 ch = p.getCharacteristics(uuid = UUID(device_char_UUID))[0]
 
+#store results in text format
 result_txt = open("result.txt", "w")
 
-i = 0
-while i < 10:
+#repeatedly read data from the wearable and store it into the text file
+while 1:
 	val = ch.read()
 	print(val)
 	result_txt.write(val.decode("utf-8") + "\n")
 	time.sleep(char_read_interval)
-	i = i + 1
 
 result_txt.close()
 p.disconnect()
